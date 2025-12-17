83c9fdbdf3b76650b8311072ea3619b30b515007
Kyphong Pham <130786963+Kyphong-Pham@users.noreply.github.com>
2025-12-17 12:56:26 -0500

    Finished work

--- main.ts
+++ main.ts
@@ -1,8 +1,42 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2025 Kyphong Pham All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Kyphong Pham
+ * Created on: Nov 2025
+ * This program uses the bluetooth radios
 */
 
-basic.showString('Hello, World!')
+// variables
+let distanceToObject: number = 0
+
+// setup
+radio.setGroup(247)
+basic.showIcon(IconNames.Happy)
+
+// receive
+radio.onReceivedString(function (receivedString) {
+    basic.clearScreen()
+    basic.showString(receivedString)
+    basic.showIcon(IconNames.Happy)
+})
+
+// loop
+while (true) {
+    distanceToObject = sonar.ping(
+        DigitalPin.P12,
+        DigitalPin.P13,
+        PingUnit.Centimeters
+    )
+
+    // if distance is less than 5
+    if (distanceToObject < 5) {
+        basic.clearScreen()
+        basic.showNumber(distanceToObject)
+        radio.sendString("Too close")
+        basic.pause(1000)
+    } else {
+        basic.clearScreen()
+        basic.showNumber(distanceToObject)
+        radio.sendString("")
+        basic.pause(1000)
+    }
+}
